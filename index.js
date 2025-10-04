9b410774ee8cdb50cbcbbbe27a25d7cee1436002
gurvinderji <gurvinderji2000@gmail.com>
2025-10-04 11:08:02 +0530

    yet we create file model and controller for store file and for showing and also we create api for file create and delete its also for filesystem and us fs model for file storage delete and multer for file upload and giving path

--- index.js
+++ index.js
@@ -3,20 +3,39 @@ dotenv.config();
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DB);
-// .then(() => {console.log("connected");});
 
 const express = require("express");
+const { v4: uniqueId } = require("uuid");
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, next) => {
+    next(null, "files/");
+  },
+  filename: (req, file, next) => {
+    const nameArr = file.originalname.split(".");
+    const ext = nameArr.pop();
+    const name = `${uniqueId()}.${ext}`;
+    next(null, name);
+  },
+});
+const upload = multer({ storage: storage });
+
 const { signup, login } = require("./controller/user.controller");
+const {
+  createFile,
+  fetchFiles,
+  deleteFile,
+} = require("./controller/file.controller");
 const app = express();
 app.listen(process.env.PORT || 8080);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("view"));
 
 app.post("/signup", signup);
 app.post("/login", login);
-
-// app.get("/", (req, res) => {
-//   res.send("success");
-// });
+app.post("/file", upload.single("file"), createFile);
+app.get("/file", fetchFiles);
+app.delete("/file/:id", deleteFile);
